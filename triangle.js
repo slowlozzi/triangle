d47233dd4e3fb038e06bbd2c5b948cc072a8994f
slowlozzi <123765509+slowlozzi@users.noreply.github.com>
2023-02-06 20:34:51 +0200

    Update triangle.js

--- triangle.js
+++ triangle.js
@@ -1,7 +1,12 @@
-let a = (#
-         ##
-         ###
-         ####
-         #####
-         ######)
-console.log(a)
+let a = "#"
+
+let b = "##"
+
+let c = "###"
+
+let d = "####"
+
+let e = "#####"
+
+let f = "######"
+console.log(let a + b + c + d + e + f)
